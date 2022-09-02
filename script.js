b222c1dbc0019ba3518c746ab9c59af452163f0a
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-02 20:07:11 -0300

    Requisito Obrigatório 7 e 8

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function criarTarefa() {
   document.getElementById('texto-tarefa').value = '';
 }
 
+function alterarCorLi(event) {
+  if (document.querySelector('.selecionado')) {
+    document.querySelector('.selecionado').classList.remove('selecionado');
+  }
+  const eventTarget = event.target;
+  eventTarget.classList.add('selecionado');
+}
+
 document.getElementById('criar-tarefa').addEventListener('click', criarTarefa);
+document
+  .getElementById('lista-tarefas')
+  .addEventListener('click', alterarCorLi);
