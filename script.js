fe3201d4677ffbe01a2b55c9929fc2e8f9c851d0
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-05 16:36:11 -0300

    Requisito Bônus 12

--- script.js
+++ script.js
@@ -33,6 +33,30 @@ function removerFinalizados() {
   }
 }
 
+function salvarTarefas() {
+  const item = document.querySelectorAll('li');
+  const itens = [];
+  const classe = [];
+  for (let i = 0; i < item.length; i += 1) {
+    itens.push(item[i].innerText);
+    classe.push(item[i].className);
+  }
+  localStorage.setItem('salvarTarefas', JSON.stringify(itens));
+  localStorage.setItem('salvarClasses', JSON.stringify(classe));
+}
+
+function carregarTarefas() {
+  const itensSalvos = JSON.parse(localStorage.getItem('salvarTarefas'));
+  const classesSalvas = JSON.parse(localStorage.getItem('salvarClasses'));
+  if (localStorage.getItem('salvarTarefas')) {
+    for (let i = 0; i < itensSalvos.length; i += 1) {
+      listaTarefas.appendChild(document.createElement('li')).innerText = itensSalvos[i];
+      document.querySelectorAll('li')[i].className = classesSalvas[i];
+    }
+  }
+}
+
+carregarTarefas();
 document.getElementById('criar-tarefa').addEventListener('click', criarTarefa);
 listaTarefas.addEventListener('click', alterarCorLi);
 listaTarefas.addEventListener('dblclick', riscarItem);
@@ -40,3 +64,6 @@ document.getElementById('apaga-tudo').addEventListener('click', apagaTudo);
 document
   .getElementById('remover-finalizados')
   .addEventListener('click', removerFinalizados);
+document
+  .getElementById('salvar-tarefas')
+  .addEventListener('click', salvarTarefas);
